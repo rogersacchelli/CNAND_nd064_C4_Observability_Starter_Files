6fd984c7970827b6bc1f3008d3787bc08c019425
Roger Sacchelli <roger@r-simulation.com>
2023-09-26 00:24:06 -0300

    frontend service monitoring

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template, request
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-
+metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def homepage():
     return render_template("main.html")
 
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
 
 if __name__ == "__main__":
     app.run()
